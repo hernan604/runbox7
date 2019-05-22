6736ad3972ae6e63169098c6d42fca47c7f6f17b
Hernan Lopes <hernan604@runbox.com>
2019-05-22 19:52:37 +0200

    Aliases/Edit page has an updated layout, and updated crud methods

--- src/app/profiles/profiles.edit.aliases.ts
+++ src/app/profiles/profiles.edit.aliases.ts
@@ -60,100 +60,142 @@ import {
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatSelectModule} from '@angular/material'; 
 @Component({
-    selector: 'profiles-edit-aliases',
+    selector: 'aliases-edit',
+    styles: [`
+    .mat_header {
+        padding: 10px 0 10px 10px
+    }
+    .mat_card.update .mat_header,
+    .mat_card.create .mat_header {
+        background-color: #013b69;
+     }
+    .header-image {
+       border-radius: 50%;
+       flex-shrink: 0;
+       background-image:url(/_img/avatar.svg);
+       background-size: cover;
+    }
+    `],
     template: `
-        <h1 mat-dialog-title>{{is_create ? 'Create Alias' : 'Edit Alias'}}</h1>
-        <div mat-dialog-content>
-            <form>
-                <mat-form-field style="margin: 10px;">
-                    <input matInput placeholder="Localpart" 
-                        name="localpart"
-                        [readonly]="!is_create"
-                        [(ngModel)]="data.localpart"
-                        (ngModelChange)="onchange_field('localpart')"
-                    >
-                    <div *ngIf="field_errors && field_errors.localpart">
-                        <mat-hint>
-                            ie. My main profile
-                        </mat-hint>
-                        <mat-error *ngFor="let error of field_errors.localpart; let i = index;">
-                            {{error}}
-                        </mat-error>
-                    </div>
-                </mat-form-field>
-                @
-                <mat-form-field style="margin: 10px;" *ngIf="is_create">
-                    <mat-label>Domain</mat-label>
-                    <mat-select
-                        name="domain"
-                        [disabled]="!is_create"
-                        [(ngModel)]="data.domain"
-                        (ngModelChange)="onchange_field('domain')"
-                    >
-                        <mat-option *ngFor="let domain of allowed_domains" [value]="domain">
-                            {{domain}}
-                        </mat-option>
-                    </mat-select>
-                    <div *ngIf="field_errors && field_errors.domain">
-                        <mat-hint>
-                            ie. jamesbond@runbox.com
-                        </mat-hint>
-                        <mat-error *ngFor="let error of field_errors.domain; let i = index;">
-                            {{error}}
-                        </mat-error>
-                    </div>
-                </mat-form-field>
-                <mat-form-field style="margin: 10px;" *ngIf="!is_create">
-                    <input matInput placeholder="Domain"
-                        name="domain"
-                        readonly
-                        [(ngModel)]="data.domain"
-                        (ngModelChange)="onchange_field('domain')"
-                    >
-                </mat-form-field>
-                <mat-form-field style="margin: 10px;">
-                    <input matInput placeholder="Deliver to account"
-                        name="forward_to"
-                        [(ngModel)]="data.forward_to"
-                        (ngModelChange)="onchange_field('forward_to')"
-                    >
-                    <mat-hint>
-                        ie. you@runbox.com
-                    </mat-hint>
-                    <div *ngIf="field_errors && field_errors.forward_to">
-                        <mat-hint>
-                            ie. James Bond
-                        </mat-hint>
-                        <mat-error *ngFor="let error of field_errors.forward_to; let i = index;">
-                            {{error}}
-                        </mat-error>
-                    </div>
-                </mat-form-field>
-            </form>
-        </div>
-        <div mat-dialog-actions>
-          <button mat-raised-button (click)="save()">Save</button>
-          <button mat-raised-button (click)="close()">Close</button>
-        </div>
-        <mat-divider></mat-divider>
-        <pre>{{data | json}}</pre>
+        <mat-card class="mat_card {{css_class()}}" style="padding: 0px">
+          <mat-card-header class="mat_header">
+              <div mat-card-avatar class="header-image" >
+              </div>
+              <mat-card-title >
+                  <div *ngIf="is_create" style="color: #FFF;">Create alias</div>
+                  <div *ngIf="is_update" style="color: #FFF;">{{data.localpart + '@' + data.domain}}</div>
+                  <div *ngIf="is_delete" style="color: #000;">Delete alias {{data.localpart + '@' + data.domain}} ?</div>
+              </mat-card-title>
+              <mat-card-subtitle>
+                  <div *ngIf="is_update" style="color: #FFF;">Forwards to: {{data.forward_to}}</div>
+                  <div *ngIf="is_delete" style="color: #000;">Forwards to: {{data.forward_to}}</div>
+              </mat-card-subtitle>
+          </mat-card-header>
+          <mat-card-content>
+                <div mat-dialog-content>
+                    <form>
+                        <mat-form-field style="margin: 10px; width: 40%;">
+                            <input matInput placeholder="Localpart" 
+                                name="localpart"
+                                [readonly]="!is_create"
+                                [(ngModel)]="data.localpart"
+                                (ngModelChange)="onchange_field('localpart')"
+                            >
+                            <div *ngIf="field_errors && field_errors.localpart">
+                                <mat-hint>
+                                    ie. My main profile
+                                </mat-hint>
+                                <mat-error *ngFor="let error of field_errors.localpart; let i = index;">
+                                    {{error}}
+                                </mat-error>
+                            </div>
+                        </mat-form-field>
+                        @
+                        <mat-form-field style="margin: 10px; width: 40%;" *ngIf="is_create">
+                            <mat-label>Domain</mat-label>
+                            <mat-select
+                                name="domain"
+                                [disabled]="!is_create"
+                                [(ngModel)]="data.domain"
+                                (ngModelChange)="onchange_field('domain')"
+                            >
+                                <mat-option *ngFor="let domain of allowed_domains" [value]="domain">
+                                    {{domain}}
+                                </mat-option>
+                            </mat-select>
+                            <div *ngIf="field_errors && field_errors.domain">
+                                <mat-hint>
+                                    ie. jamesbond@runbox.com
+                                </mat-hint>
+                                <mat-error *ngFor="let error of field_errors.domain; let i = index;">
+                                    {{error}}
+                                </mat-error>
+                            </div>
+                        </mat-form-field>
+                        <mat-form-field style="margin: 10px; width: 45%;" *ngIf="!is_create">
+                            <input matInput placeholder="Domain"
+                                name="domain"
+                                readonly
+                                [(ngModel)]="data.domain"
+                                (ngModelChange)="onchange_field('domain')"
+                            >
+                        </mat-form-field>
+                        <mat-form-field style="margin: 10px; width: 95%;">
+                            <input matInput placeholder="Deliver to account"
+                                name="forward_to"
+                                [(ngModel)]="data.forward_to"
+                                (ngModelChange)="onchange_field('forward_to')"
+                            >
+                            <mat-hint>
+                                ie. you@runbox.com
+                            </mat-hint>
+                            <div *ngIf="field_errors && field_errors.forward_to">
+                                <mat-hint>
+                                    ie. James Bond
+                                </mat-hint>
+                                <mat-error *ngFor="let error of field_errors.forward_to; let i = index;">
+                                    {{error}}
+                                </mat-error>
+                            </div>
+                        </mat-form-field>
+                    </form>
+                </div>
+          </mat-card-content>
+          <mat-card-actions style="padding: 0px 10px">
+            <button mat-raised-button (click)="save()" color="primary" *ngIf="!is_delete">SAVE</button>
+            <button mat-raised-button (click)="delete()" color="" *ngIf="is_delete">DELETE</button>
+            <button mat-raised-button (click)="close()" color="warn">CANCEL</button>
+          </mat-card-actions>
+          <mat-card-footer>
+          </mat-card-footer>
+        </mat-card>
     `
 })
 
-export class ProfilesEditAliases {
+export class AliasesEdit {
     @Input() value: any[];
     field_errors;
-    is_create = false; //indicates if its create or update
+    is_create = false;
+    is_update = false;
+    is_delete = false;
+    has_deleted = false;
+    has_updated = false;
+    has_created = false;
     allowed_domains = [];
     constructor(
         private http: Http,
         public snackBar: MatSnackBar,
-        public dialog_ref: MatDialogRef<ProfilesEditAliases>,
-        @Inject(MAT_DIALOG_DATA) public data: any
+        public dialog_ref: MatDialogRef<AliasesEdit>,
+        @Inject(MAT_DIALOG_DATA) 
+        public data: any,
     ) {
-        if ( ! data || ! Object.keys(data).length ) { this.is_create = true }
-        console.log('received data', data)
         this.load_allowed_domains();
+    }
+    css_class() {
+        let css = '';
+        if ( this.is_delete ) { return 'delete' }
+        else if ( this.is_create ) { return 'create' }
+        else if ( this.is_update ) { return 'update' }
     }
     load_allowed_domains() {
         let req = this.http.get('/rest/v1/alias/allowed_domains')
@@ -162,15 +204,12 @@ export class ProfilesEditAliases {
         req.subscribe(
           data => {
             const reply = data.json();
-            console.log('allowed_domains', reply)
             this.allowed_domains = reply.result.allowed_domains;
             return;
           },
           error => {
-            return this.show_error('Could not load profiles.', 'Dismiss');
+            return this.show_error('Could not load allowed domains.', 'Dismiss');
           }
-
-
         );
     }
     save() {
@@ -179,13 +218,13 @@ export class ProfilesEditAliases {
     }
     create(){
         let data = this.data;
-        this.http.post('/rest/v1/alias/', {
+        let req = this.http.post('/rest/v1/alias/', {
             localpart : data.localpart,
             domain : data.domain,
             forward_to : data.forward_to,
         })
-        .pipe(timeout(60000))
-        .subscribe(
+        req.pipe(timeout(60000))
+        req.subscribe(
           data => {
             const reply = data.json();
             if ( reply.status == 'error' ) {
@@ -197,22 +236,35 @@ export class ProfilesEditAliases {
                 }
                 return;
             }
-            this.close();
-            console.log('profiles', reply)
-            return;
+            this.has_created = true;
+            return this.close();
           },
           error => {
             return this.show_error('Could not load profiles.', 'Dismiss');
           }
         );
     }
+    delete() {
+        let data = this.data;
+        let req = this.http.delete('/rest/v1/alias/'+data.id)
+        req.pipe(timeout(5000))
+        req.subscribe(data => {
+            const reply = data.json();
+            if ( reply.status == 'success' ) {
+                this.has_deleted = true;
+                return this.close();
+            } else {
+                this.show_error('Could not delete alias.', 'Dismiss');
+            }
+        })
+    }
     update(){
         let data = this.data;
-        this.http.put('/rest/v1/alias/'+data.id, {
+        let req = this.http.put('/rest/v1/alias/'+data.id, {
             forward_to : this.data.forward_to,
         })
-        .pipe(timeout(60000))
-        .subscribe(
+        req.pipe(timeout(60000))
+        req.subscribe(
           data => {
             const reply = data.json();
             if ( reply.status == 'error' ) {
@@ -224,9 +276,8 @@ export class ProfilesEditAliases {
                 }
                 return;
             }
-            this.close();
-            console.log('profiles', reply)
-            return;
+            this.has_updated = true;
+            return this.close();
           },
           error => {
             return this.show_error('Could not load profiles.', 'Dismiss');
