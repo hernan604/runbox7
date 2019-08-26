06539c3d366863983f18f301a30ba5f5c4ae1928
Hernan Lopes <hernan604@runbox.com>
2019-08-26 20:07:04 +0200

    profile interface validate email update

--- src/app/aliases/aliases.lister.ts
+++ src/app/aliases/aliases.lister.ts
@@ -55,6 +55,7 @@ import {
 } from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {AliasesEditorModal} from '../aliases/aliases.editor.modal';
+import {RMM} from '../rmm';
 
 @Component({
     selector: 'aliases-lister',
@@ -64,8 +65,8 @@ import {AliasesEditorModal} from '../aliases/aliases.editor.modal';
             position: relative;
             margin: 10px;
         }
-        .aliases-lister form > div > div {
-            overflow: scroll;
+        mat-divider.transparent {
+            border-color: transparent;
         }
     `],
     template: `
@@ -74,45 +75,88 @@ import {AliasesEditorModal} from '../aliases/aliases.editor.modal';
         <ng-content select="[section-description]"></ng-content>
         <ng-content select="[section-buttons]"></ng-content>
         <form *ngFor="let item of values; let i = index;">
-            <mat-divider *ngIf="i > 0"></mat-divider>
+            <div style='width: 100%'>
+                <mat-divider *ngIf="i > 0"></mat-divider>
 
-            <mat-form-field class="alias" style="margin: 10px;">
-                <input
-                    matInput
-                    placeholder="Alias"
-                    readonly="true"
-                    [value]="item.localpart + '@' + item.domain"
+                <mat-form-field class="alias" style="margin: 10px;">
+                    <input
+                        matInput
+                        placeholder="Alias"
+                        readonly="true"
+                        [value]="item.localpart + '@' + item.domain"
+                    >
+                </mat-form-field>
+
+                <mat-form-field class="forward_to" style="margin: 10px;">
+                    <input
+                        matInput
+                        placeholder="Deliver to Account"
+                        readonly="true"
+                        [value]="item.forward_to"
+                    >
+                </mat-form-field>
+
+                <button 
+                    (click)="edit(item)" 
+                    color='primary' 
+                    mat-raised-button 
+                    style="margin: 10px;"
                 >
-            </mat-form-field>
+                    Edit
+                </button>
 
-            <mat-form-field class="forward_to" style="margin: 10px;">
-                <input
-                    matInput
-                    placeholder="Deliver to Account"
-                    readonly="true"
-                    [value]="item.forward_to"
+                <button 
+                    (click)="delete(i, item)"
+                    *ngIf="!is_delete_disabled"
+                    color='primary' 
+                    mat-raised-button
+                    style="margin: 10px;"
                 >
-            </mat-form-field>
+                    Delete
+                </button>
+                    <button 
+                        *ngIf="!is_visible_code_check(item)"
+                        (click)="set_visible_code_check(item, true)" 
+                        color='primary' 
+                        mat-raised-button 
+                        style="margin: 10px;"
+                    >
+                        Validate code
+                    </button>
+            </div>
+            <mat-divider class='transparent'></mat-divider>
+            <div *ngIf="!item.is_email_verified" style=''>
+                <div *ngIf="is_visible_code_check(item)" style='background: white; border: 1px solid #CCC'>
+                    <mat-form-field class="email_validation_code" style="margin: 10px;">
+                        <input
+                            matInput
+                            placeholder="Enter aliases validation code"
+                            [value]="item.validation_code"
+                            [(ngModel)]="item.validation_code"
+                            [ngModelOptions]="{standalone: true}"
+                        >
+                    </mat-form-field>
 
-            <button 
-                (click)="edit(item)" 
-                color='primary' 
-                mat-raised-button 
-                style="margin: 10px;"
-            >
-                Edit
-            </button>
+                    <button 
+                        [disabled]="is_validation_code_set(item)"
+                        (click)="validate_email(item)" 
+                        color='primary' 
+                        mat-raised-button 
+                        style="margin: 10px;"
+                    >
+                        Validate
+                    </button>
+                    <p>
+                        Check your email <strong>{{item.localpart + '@' + item.domain}})</strong> for the validation code.
+                    </p>
 
-            <button 
-                (click)="delete(i, item)"
-                *ngIf="!is_delete_disabled"
-                color='primary' 
-                mat-raised-button
-                style="margin: 10px;"
-            >
-                Delete
-            </button>
-
+                </div>
+                <div *ngIf="!is_visible_code_check(item)">
+                        Alias not verified.
+                        {{item | json}}
+                        Check your email on runbox for the aliases validation code and enter <a (click)="set_visible_code_check(item, true)" href='javascript:void(0)'>here</a>.
+                </div>
+            </div>
         </form>
     </div>
 
@@ -123,8 +167,10 @@ export class AliasesLister {
   @Input() is_delete_disabled: false;
   @Output() ev_reload = new EventEmitter<string>();
   private dialog_ref : any;
+  visible_code_check = {};
   constructor(public dialog: MatDialog,
     public snackBar: MatSnackBar,
+    public rmm: RMM,
   ) {}
   edit (item): void {
       item = JSON.parse(JSON.stringify(item))
@@ -152,6 +198,31 @@ export class AliasesLister {
       //    this.ev_reload.emit('deleted');
       //}
     });
+  }
+  validate_email (item): void {
+      const data = JSON.parse(JSON.stringify(item))
+      let req = this.rmm.alias.validate({
+          validation_code : data.validation_code,
+      })
+      req.subscribe((data)=>{
+          let reply = data.json();
+          console.log('VALIDATION REPLY', reply)
+          if ( reply.status == 'success' ) {
+              item.is_email_verified = 1;
+          } else {
+              this.show_error('Could not validate aliases.', 'Dismiss');
+          }
+      })
+  }
+  is_validation_code_set (item) {
+    if (item && item.validation_code && item.validation_code.match(/^.{256}$/)) { return true; }
+    return false;
+  }
+  is_visible_code_check (item) {
+    return item && this.visible_code_check[item.id];
+  }
+  set_visible_code_check (item, is_visible) {
+    this.visible_code_check[item.id] = is_visible
   }
   show_error (message, action) {
     this.snackBar.open(message, action, {
